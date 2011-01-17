5f9b9f96e51140a58e074da4e529c930074c6959
Rohan McGovern <rohan.mcgovern@nokia.com>
2011-01-17 14:36:06 +1000

    puppet: fix puppet module for OpenSuSE

--- puppet/modules/puppet/manifests/init.pp
+++ puppet/modules/puppet/manifests/init.pp
@@ -1,12 +1,12 @@
 import '*'
 
 class puppet {
-
     case $operatingsystem {
         Darwin:     { include puppet::mac }
         Ubuntu:     { include puppet::ubuntu }
         CentOS:     { include puppet::centos }
         SuSE:       { include puppet::unix }
+        OpenSuSE:   { include puppet::unix }
         Solaris:    { include puppet::solaris }
     }
 }
