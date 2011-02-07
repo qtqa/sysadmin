d2a146a0c826c126aa3eb35c03efde8fe9f06231
Rohan McGovern <rohan.mcgovern@nokia.com>
2011-02-07 13:01:18 +1000

    baselayout: give a helpful failure message about $testuser on mac.
    We can't safely create the test user account on Mac via puppet.
    
    Add a `fail' with a helpful error message to make it clear that this
    user account setup must be done outside of puppet.
    
    Reviewed-by: Sunil Thaha
    Reviewed-by: Kalle Lehtonen
    
    Change-Id: I28748f27ee145357d0ce16f5493cdb97670fb2a9

--- puppet/modules/baselayout/manifests/mac.pp
+++ puppet/modules/baselayout/manifests/mac.pp
@@ -1,5 +1,34 @@
 class baselayout::mac inherits baselayout::unix {
 
+    if $testuser {
+        # On Mac, we can't safely create the test user ourselves.
+        #
+        # The puppet Mac user provider does not seem to set up everything
+        # in the same way as setting up through the UI (for example, it lacks
+        # `managehome' feature).
+        #
+        # Also, there doesn't seem to be a cleanly scriptable way to enable
+        # automated login. Therefore, we'll output to syslog that the
+        # maintainer of this test machine must manually set up the test
+        # user before we can continue.
+
+        $user_exists = generate('/bin/sh', '-c', " \
+            if /bin/test -d /Users/$testuser; then \
+                /bin/echo -n ok;                   \
+            else                                   \
+                /bin/echo -n nok;                  \
+            fi                                     \
+        ")
+
+        if $user_exists == "nok" {
+            fail("                                                \
+Sorry, some manual setup must be done before puppet can continue! \
+Please use the GUI to create a user named `$testuser' and enable  \
+automatic login for that user.                                    \
+            ")
+        }
+    }
+
     File {
         owner   =>  "root",
         group   =>  "wheel",
