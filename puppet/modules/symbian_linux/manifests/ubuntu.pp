dc02c1be8ef36450569308b0e417c4e6f650ca1f
Rohan McGovern <rohan.mcgovern@nokia.com>
2011-02-22 10:45:55 +1000

    symbian_linux: add new module
    Allows for compilation of Qt using symbian-gcce, symbian-armcc mkspecs
    on Linux.
    
    Reviewed-by: Kalle Lehtonen
    Task: QTQAINFRA-18
    Task: QTQAINFRA-19
    Change-Id: I48873e9d5a0938a09691e6429704f37b13684d28

---
+++ puppet/modules/symbian_linux/manifests/ubuntu.pp
@@ -0,0 +1,26 @@
+class symbian_linux::ubuntu
+{
+    exec { "install RVCT under /opt":
+        command     =>  "/bin/sh -c 'wget $input/symbian-linux/rvct-2.2.686.tar.gz -O - | tar -C /opt -xvz'",
+        creates     =>  "/opt/rvct-2.2.686/bin",
+        logoutput   =>  true,
+    }
+
+    exec { "install gcce":
+        command     =>  "/bin/sh -c '
+
+        wget $input/symbian-linux/gcce-4.4.172-r1.deb -O /tmp/gcce.deb  \
+            && dpkg --install /tmp/gcce.deb                             \
+            && rm -f /tmp/gcce.deb
+
+        '",
+        creates     =>  "/usr/bin/arm-none-symbianelf-gcc-4.4.1",
+        logoutput   =>  true,
+    }
+
+    # wine is used to run the .exe files from the S60 SDKs
+    package { "wine1.2":
+        ensure  =>  present,
+    }
+}
+
