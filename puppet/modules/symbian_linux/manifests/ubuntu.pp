6925b37caf9d3803d449f928db223050a60d7447
Rohan McGovern <rohan.mcgovern@nokia.com>
2011-04-05 09:47:50 +1000

    symbian_linux: fixed build hang when elf2e32.exe crashes
    By default, wine will raise an interactive crash dialog for any
    crashing executable.  This is not amenable to test automation; it
    causes any build process to hang, since nothing will dismiss the dialog.
    
    Tweak the registry to disable this crash dialog, so builds will fail
    rather than hang when an .exe crashes.
    
    Reviewed-by: Toby Tomkins
    Change-Id: Ifaa17d6f473eec381e5f4ae713213b2222f9129b

--- puppet/modules/symbian_linux/manifests/ubuntu.pp
+++ puppet/modules/symbian_linux/manifests/ubuntu.pp
@@ -24,6 +24,38 @@ class symbian_linux::ubuntu
     }
 
     if $testuser {
+        # elf2e32, or any other .exe files from the SDK, may crash.
+        # By default this would pop up an interactive crash dialog, which is
+        # unacceptable for us.  This will disable that dialog.
+        #
+        # Gleaned from: http://wiki.winehq.org/FAQ#head-c857c433cf9fc1dcd90b8369ef75c325483c91d6
+        #
+        # Note that we are not attempting to accurately parse the registry
+        # (e.g. using regedit).  In theory, this could cause problems if
+        # the registry contains some unusual content.
+        # In practice, I feel that this is a better option than running
+        # regedit at each puppet run, as that would instantiate the entire
+        # wine environment (e.g. will launch wineserver) even if the test
+        # machine is not intending to run any wine apps.  It's hard to predict
+        # what unusual problems that may cause.
+        #
+        $user_reg = "/home/$testuser/.wine/user.reg"
+        exec { "disable wine crash dialog":
+
+            # correct backslash counts were determined by experimentation
+            command =>  "/bin/sh -c '
+                {
+                    echo;
+                    /bin/echo -E [Software\\\\\\\\Wine\\\\\\\\WineDbg];
+                    /bin/echo -E \\\"ShowCrashDialog\\\"=dword:00000000;
+                } >> $user_reg
+            '",
+
+            onlyif  =>  "/bin/sh -c '
+                test -f $user_reg && ! grep -q \\\"ShowCrashDialog\\\"=dword:00000000 $user_reg
+            '",
+        }
+
         # This wrapper allows armcc and ccache to work nicely together.
         file { "/home/$testuser/bin/armcc":
             source  =>  "puppet:///modules/symbian_linux/armcc_ccache",
