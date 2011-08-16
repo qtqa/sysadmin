b9d8f59149419597866f0fbaea0e2a92c8f1921e
Rohan McGovern <rohan.mcgovern@nokia.com>
2011-08-16 15:44:44 +1000

    mac: added bootstrap script for OSX 10.7
    Differences between this and OSX 10.6 bootstrap:
    
      - jni.h workaround is no longer needed (the file is already present on
        OSX 10.7)
      - git is now provided by xcode, at /usr/bin/git, so we don't need to
        install it by macports
      - don't automatically install macports and xcode, make the user
        install it manually - mainly because the correct versions to use are
        undefined
    
    Reviewed-by: Sergio Ahumada
    Change-Id: I5cd6e7137d2a23a129f422f89a5fee9b0e381522

---
+++ bootstrap/mac_107_bootstrap.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+# Bootstrap a clean OSX 10.7 system to be managed by puppet.
+# NOTE: because OSX does not ship with a compiler by default, and installing
+# puppet via macports needs a compiler, this script may be a bit prone
+# to failure if you do not install gcc and macports yourself first.
+
+REPO="$1"
+if [ "x$REPO" = "x" ]; then
+    {
+        echo "Usage: $(basename $0) git://some/git/repo"
+        echo ""
+        echo "Set up this machine to be managed using the puppet config in the given"
+        echo "git repository (e.g. git://qt.gitorious.org/qtqa/sysadmin.git)"
+    } 1>&2
+    exit 2
+fi
+
+WORKDIR=$HOME/bootstrap_tmp
+INPUT=http://bq-qastore.apac.nokia.com/public/input/mac
+
+set -e
+#set -x
+
+mkdir -p $WORKDIR
+cd $WORKDIR
+
+# Ensures xcode is installed.
+# xcode is required for using macports.
+if ! gcc -v > /dev/null 2>&1; then
+    echo install xcode first 1>&2
+    exit 2
+else
+    echo xcode is already installed
+fi
+
+# Ensures macports is installed.
+# macports is required for installing puppet.
+if ! test -e /opt/local/bin/port; then
+    echo install macports first 1>&2
+    exit 2
+else
+    echo macports is already installed
+fi
+
+# Ensures puppet is installed.
+if ! test -e /opt/local/bin/puppet; then
+    echo Installing puppet...
+    /opt/local/bin/port install puppet
+else
+    echo puppet is already installed
+fi
+
+if ! test -d /var/qtqa/sysadmin; then
+    echo "Grabbing $REPO ..."
+    mkdir -p /var/qtqa
+    git clone "$REPO" /var/qtqa/sysadmin
+fi
+
+# Run puppet once.
+# From this point on, all setup of this machine is done via puppet.
+echo "Running puppet..."
+/usr/bin/env PATH=/opt/local/bin:$PATH /var/qtqa/sysadmin/puppet/sync_and_run.sh
+set +x
+echo 'All done :-)'
+echo 'If this host already has an entry in manifests/nodes.pp, nothing needs to be done...'
+echo "Otherwise, puppet is ready to go but will not do anything until you add an entry for $(/opt/local/bin/facter fqdn) to manifests/nodes.pp"
+
