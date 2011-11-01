89de6bdfccd9f744d6c124a5af543ce4094d6c19
Rohan McGovern <rohan.mcgovern@nokia.com>
2011-11-02 09:16:36 +1000

    baselayout: rearranged the Ubuntu 10.04, 11.10 and common code
    Shift the code around to make it clearer which code applies to which
    version(s) of Ubuntu.
    
    Change-Id: I06953fa1fb0bad579b6de68d7e13550886684106
    Reviewed-by: Toby Tomkins <toby.tomkins@nokia.com>

--- puppet/modules/baselayout/manifests/ubuntu.pp
+++ puppet/modules/baselayout/manifests/ubuntu.pp
@@ -1,49 +1,9 @@
 class baselayout::ubuntu inherits baselayout::linux {
 
-    # Do not use KDE since Ubuntu 11
-     if $lsbmajdistrelease < 11 {
-         $use_kde = 1
-     } else {
-         package {
-             "ubuntu-desktop":      ensure => installed;
-             "dconf-tools":         ensure => installed;
-         }
-     }
-
-    file {
-        "/etc/profile.d/99homepath.sh":
-            owner   =>  "root",
-            mode    =>  0444,
-            source  =>  "puppet:///modules/baselayout/ubuntu/profile.d/99homepath.sh",
-        ;
-    }
-
-    file {
-        "/etc/default/maybe_wait_for_real_hostname":
-            owner   =>  "root",
-            mode    =>  0444,
-            source  =>  "puppet:///modules/baselayout/ubuntu/maybe_wait_for_real_hostname",
-        ;
-        "/etc/default/locale":
-            owner   =>  "root",
-            mode    =>  0444,
-            source  =>  "puppet:///modules/baselayout/ubuntu/locale",
-            require =>  File["/etc/default/maybe_wait_for_real_hostname"],
-        ;
-        "/etc/sysctl.conf":
-            owner   =>  "root",
-            mode    =>  0444,
-            source  =>  "puppet:///modules/baselayout/ubuntu/sysctl.conf",
-            notify  =>  Exec["sysctl update"],
-        ;
-    }
-
-    exec { "sysctl update":
-        command     => "/sbin/sysctl -p",
-        refreshonly => true,
-    }
-
-    if $use_kde {
+    #========================== Ubuntu 10.04 stuff ============================
+    # This logic only applies to Ubuntu 10.04, where we use KDE.
+    #
+    if $lsbmajdistrelease < 11 {
         file {
             "/etc/X11/default-display-manager":
                 owner   =>  "root",
@@ -51,22 +11,38 @@ class baselayout::ubuntu inherits baselayout::linux {
                 content =>  "/usr/bin/kdm",
             ;
         }
-    }
 
-    # Hostname will only be set via DHCP if /etc/hostname is absent.
-    # Gleaned from https://bugs.launchpad.net/ubuntu/+source/dhcp3/+bug/90388
-    # Therefore, we remove /etc/hostname iff resolving our fqdn does not
-    # result in the correct IP address - we should then get the correct fqdn on
-    # next boot.
-    # Note: we used to remove this file at every boot, but that seems to make the
-    # boot sequence a bit racy, as some services don't like it when the hostname
-    # changes after they've started.
-    exec { "/bin/rm -f /etc/hostname":
-        onlyif => "/bin/sh -c \"[ x$(host $fqdn | sed -r -n -e 's/^.*has address //p') != x$ipaddress ]\""
-    }
+        # Hostname will only be set via DHCP if /etc/hostname is absent.
+        # Gleaned from https://bugs.launchpad.net/ubuntu/+source/dhcp3/+bug/90388
+        # Therefore, we remove /etc/hostname iff resolving our fqdn does not
+        # result in the correct IP address - we should then get the correct fqdn on
+        # next boot.
+        # Note: we used to remove this file at every boot, but that seems to make the
+        # boot sequence a bit racy, as some services don't like it when the hostname
+        # changes after they've started.
+        exec { "/bin/rm -f /etc/hostname":
+            onlyif => "/bin/sh -c \"[ x$(host $fqdn | sed -r -n -e 's/^.*has address //p') != x$ipaddress ]\""
+        }
 
-    if $testuser {
-        if $use_kde {
+        # Deployment of these files forces the boot process to stall until the correct hostname
+        # has been set.  Various services don't like it if the hostname changes after they have
+        # been launched.
+        file {
+            "/etc/default/maybe_wait_for_real_hostname":
+                owner   =>  "root",
+                mode    =>  0444,
+                source  =>  "puppet:///modules/baselayout/ubuntu/maybe_wait_for_real_hostname",
+            ;
+            "/etc/default/locale":
+                owner   =>  "root",
+                mode    =>  0444,
+                source  =>  "puppet:///modules/baselayout/ubuntu/locale",
+                require =>  File["/etc/default/maybe_wait_for_real_hostname"],
+            ;
+        }
+
+        if $testuser {
+            # This kdmrc forces autologin of $testuser
             file {
                 "/etc/kde4/kdm/kdmrc":
                     owner   =>  "root",
@@ -74,8 +50,21 @@ class baselayout::ubuntu inherits baselayout::linux {
                     content =>  template("baselayout/kdmrc.ubuntu.erb")
                 ;
             }
-        } else {
+        }
+    }
+
+    #========================== Ubuntu 11.10 stuff ============================
+    # This logic only applies to Ubuntu 11.10, where we use Gnome.
+    #
+    if $lsbmajdistrelease >= 11 {
+        package {
+            "ubuntu-desktop":   ensure => installed;
+            "dconf-tools":      ensure => installed;
+        }
+
+        if $testuser {
             file {
+                # This enables autologin as $testuser
                 "/etc/lightdm/lightdm.conf":
                     owner   =>  "root",
                     mode    =>  0444,
@@ -105,4 +94,27 @@ class baselayout::ubuntu inherits baselayout::linux {
             }
         }
     }
+
+    #========================== common stuff ==================================
+    # Applies to all supported Ubuntu versions
+    #
+    file {
+        "/etc/profile.d/99homepath.sh":
+            owner   =>  "root",
+            mode    =>  0444,
+            source  =>  "puppet:///modules/baselayout/ubuntu/profile.d/99homepath.sh",
+        ;
+        "/etc/sysctl.conf":
+            owner   =>  "root",
+            mode    =>  0444,
+            source  =>  "puppet:///modules/baselayout/ubuntu/sysctl.conf",
+            notify  =>  Exec["sysctl update"],
+        ;
+    }
+
+    exec { "sysctl update":
+        command     => "/sbin/sysctl -p",
+        refreshonly => true,
+    }
+
 }
