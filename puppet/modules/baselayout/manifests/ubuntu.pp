f6c475b573f9142a3671c370e7b19322a541d1ac
Rohan McGovern <rohan.mcgovern@nokia.com>
2012-06-08 09:49:38 +1000

    baselayout: apply /etc/hostname fix also on Ubuntu 12.04
    This is necessary to ensure /etc/hostname is set according to DHCP.
    The same workaround used for Ubuntu 10.04 also works for Ubuntu 12.04.
    The workaround used for Ubuntu 11.10 does not work on 12.04.
    
    Change-Id: I534e959e2fbffa13e045fbe74a66e3c98eb4aeb7
    Reviewed-by: Kalle Lehtonen <kalle.ju.lehtonen@nokia.com>

--- puppet/modules/baselayout/manifests/ubuntu.pp
+++ puppet/modules/baselayout/manifests/ubuntu.pp
@@ -12,18 +12,6 @@ class baselayout::ubuntu inherits baselayout::linux {
             ;
         }
 
-        # Hostname will only be set via DHCP if /etc/hostname is absent.
-        # Gleaned from https://bugs.launchpad.net/ubuntu/+source/dhcp3/+bug/90388
-        # Therefore, we remove /etc/hostname iff resolving our fqdn does not
-        # result in the correct IP address - we should then get the correct fqdn on
-        # next boot.
-        # Note: we used to remove this file at every boot, but that seems to make the
-        # boot sequence a bit racy, as some services don't like it when the hostname
-        # changes after they've started.
-        exec { "/bin/rm -f /etc/hostname":
-            onlyif => "/bin/sh -c \"[ x$(host $fqdn | sed -r -n -e 's/^.*has address //p') != x$ipaddress ]\""
-        }
-
         # Deployment of these files forces the boot process to stall until the correct hostname
         # has been set.  Various services don't like it if the hostname changes after they have
         # been launched.
@@ -113,6 +101,23 @@ class baselayout::ubuntu inherits baselayout::linux {
             }
         }
     }
+
+    #========================= hostname fix ===================================
+    # This workaround seems required on Ubuntu 10.04 and 12.04, but not 11.10
+    if $lsbmajdistrelease != 11 {
+        # Hostname will only be set via DHCP if /etc/hostname is absent.
+        # Gleaned from https://bugs.launchpad.net/ubuntu/+source/dhcp3/+bug/90388
+        # Therefore, we remove /etc/hostname iff resolving our fqdn does not
+        # result in the correct IP address - we should then get the correct fqdn on
+        # next boot.
+        # Note: we used to remove this file at every boot, but that seems to make the
+        # boot sequence a bit racy, as some services don't like it when the hostname
+        # changes after they've started.
+        exec { "/bin/rm -f /etc/hostname":
+            onlyif => "/bin/sh -c \"[ x$(host $fqdn | sed -r -n -e 's/^.*has address //p') != x$ipaddress ]\""
+        }
+    }
+
 
     #========================== common stuff ==================================
     # Applies to all supported Ubuntu versions
