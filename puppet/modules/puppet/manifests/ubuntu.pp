a5bd14b2b437710ca5f4ecf88638e1d68168f859
Rohan McGovern <rohan.mcgovern@nokia.com>
2012-07-02 10:51:54 +1000

    puppet: upgrade puppet to 2.7 on Ubuntu 10.04 machines
    The version of puppet available out of the box on Ubuntu 10.04 is
    painfully old; use the lucid-backports repository to upgrade it.
    
    The primary benefit of the upgrade is that we now have a sufficiently
    new puppet everywhere to be able to use parameterized classes.
    
    Change-Id: I13268e5fc99f2f571958fdff1b67f63bd60c3ce6
    Reviewed-by: Kalle Lehtonen <kalle.ju.lehtonen@nokia.com>

--- puppet/modules/puppet/manifests/ubuntu.pp
+++ puppet/modules/puppet/manifests/ubuntu.pp
@@ -1,2 +1,21 @@
 class puppet::ubuntu inherits puppet::unix {
+
+    # On Lucid, use backports to get a newer puppet.
+    if $lsbdistcodename == 'lucid' {
+        include ubuntu_backports
+
+        file { "/etc/apt/preferences.d/lucid-backports-puppet.conf":
+            source => "puppet:///puppet/lucid-backports-puppet.conf"
+        }
+
+        Package {
+            require => File[ "/etc/apt/sources.list.d/lucid-backports-puppet.conf" ]
+        }
+
+        package {
+            "puppet": ensure => latest;
+            "puppet-common": ensure => latest;
+        }
+    }
+
 }
---
+++ puppet/modules/ubuntu_backports/manifests/init.pp
@@ -0,0 +1,16 @@
+class ubuntu_backports {
+    file {
+        "/etc/apt/sources.list.d/$lsbdistcodename-backports.list":
+            content => template("ubuntu_backports/backports.list.erb"),
+            require => File["/etc/apt/preferences.d/$lsbdistcodename-backports.conf"],
+            notify => Exec["apt-get update for ubuntu_backports"];
+        "/etc/apt/preferences.d/$lsbdistcodename-backports.conf":
+            content => template("ubuntu_backports/backports.conf.erb");
+    }
+
+    exec {
+        "apt-get update for ubuntu_backports":
+            command => "/usr/bin/apt-get update",
+            refreshonly => true;
+    }
+}
