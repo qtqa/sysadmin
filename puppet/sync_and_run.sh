81706b1b67739eccdb3b96946a589597b27a1db7
Rohan McGovern <rohan.mcgovern@nokia.com>
2012-07-18 14:12:14 +1000

    Removed old sync_and_run.sh
    This script should no longer be used anywhere.
    
    Change-Id: I88d997bf1f3584ad4c645c3e949f397e8cee990e
    Reviewed-by: Sergio Ahumada <sergio.ahumada@nokia.com>

--- puppet/sync_and_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-DIR=`dirname $0`
-
-{
-    echo "Warning: sync_and_run.sh is deprecated, please use sync_and_run.pl instead."
-    echo "This warning can be ignored if it is only seen once on this host."
-} | logger -t puppet -p daemon.warning
-
-exec "$DIR/sync_and_run.pl" "$@" 2>&1 | logger -t puppet -p daemon.error
