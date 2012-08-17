78ac1d4fe4d746963652283063a79420947fd227
Rohan McGovern <rohan.mcgovern@nokia.com>
2012-08-17 14:51:41 +1000

    baselayout: disable power management on Windows
    Don't automatically blank the screen or suspend, since this may
    interfere with test runs.
    
    Change-Id: I35d1f1361b365db2d0187f90fcc1afa8de8b3662
    Reviewed-by: Sergio Ahumada <sergio.ahumada@nokia.com>
    Reviewed-by: Toby Tomkins <toby.tomkins@nokia.com>

--- puppet/modules/baselayout/manifests/windows.pp
+++ puppet/modules/baselayout/manifests/windows.pp
@@ -23,5 +23,33 @@ class baselayout::windows inherits baselayout::base {
             ensure => absent,
         }
     }
+
+    # Avoid system suspend or screen blanking.
+    # On Windows, these magic numbers can be determined with the help of the 'powercfg' command,
+    # e.g. try 'powercfg -query'.  The GUIDs appear to be portable across systems.
+    $high_performance_guid = '8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c'
+    $schemes_path = 'HKLM\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes'
+    $display_path = "$schemes_path\\$high_performance_guid\\7516b95f-f776-4464-8c53-06167f40cc99\\3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e"
+    $sleep_path = "$schemes_path\\$high_performance_guid\\238c9fa8-0aad-41ed-83f4-97be242c8f20\\29f6c1db-86da-48c5-9fdb-f2b67b1f44da"
+
+    registry::value { "use high-performance scheme":
+        key => $schemes_path,
+        value => 'ActivePowerScheme',
+        data => $high_performance_guid
+    }
+
+    registry::value { "don't blank the screen":
+        key => $display_path,
+        value => 'ACSettingIndex',
+        type => 'dword',
+        data => '0x00000000'
+    }
+
+    registry::value { "don't sleep":
+        key => $sleep_path,
+        value => 'ACSettingIndex',
+        type => 'dword',
+        data => '0x00000000'
+    }
 }
 
