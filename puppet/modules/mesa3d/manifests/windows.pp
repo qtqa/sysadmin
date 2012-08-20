9f49175a15d1bcf9b3c03e8b7827efa4f5249462
Rohan McGovern <rohan.mcgovern@nokia.com>
2012-08-20 14:45:07 +1000

    mesa3d: partial fix for 64-bit systems
    Handle Program Files (x86) and SysWOW64 paths.
    
    The module should now work without errors, but is incomplete (it only
    deploys a 32-bit version of the DLL; it should deploy both 32-bit and
    64-bit versions).
    
    Change-Id: Id3afa1fc00c1619ab9b2bb425a3710138f99997d
    Reviewed-by: Toby Tomkins <toby.tomkins@nokia.com>

--- puppet/modules/mesa3d/manifests/windows.pp
+++ puppet/modules/mesa3d/manifests/windows.pp
@@ -1,8 +1,18 @@
 class mesa3d::windows {
+    # FIXME: this module is incomplete for 64-bit Windows.
+    # It only installs a 32-bit version of the mesa DLL.
 
     $tempdir = "c:\\temp"
+    $gitdir = $::architecture ? {
+        x64 => "C:\\Program Files (x86)\\Git",
+        default => "C:\\Program Files\\Git"
+    }
     $opengl_library = "opengl32.dll"
     $windows_system32 = "c:\\Windows\\system32"
+    $windows_dlldir32 = $::architecture ? {
+        x64 => "C:\\Windows\\SysWOW64",
+        default => $windows_system32
+    }
     $cmd = "$windows_system32\\cmd.exe"
     $mesa3ddll = "mesa3d_opengl.dll"
 
@@ -13,9 +23,9 @@ class mesa3d::windows {
     exec { prepare_for_opengl32_replace:
         command  =>  "$cmd /C \
             \
-            takeown /f $windows_system32\\$opengl_library & \
-            icacls $windows_system32\\$opengl_library /grant Administrators:F & \
-            \"C:\\Program Files\\Git\\bin\\curl.exe\" -o $tempdir\\$mesa3ddll $input/mesa3d/windows/$opengl_library\
+            takeown /f $windows_dlldir32\\$opengl_library & \
+            icacls $windows_dlldir32\\$opengl_library /grant Administrators:F & \
+            \"$gitdir\\bin\\curl.exe\" -o $tempdir\\$mesa3ddll $input/mesa3d/windows/$opengl_library\
             \
         ",
         creates => "$tempdir\\$mesa3ddll",
@@ -23,7 +33,7 @@ class mesa3d::windows {
         logoutput => true,
     }
 
-    file { "$windows_system32\\opengl32.dll":
+    file { "$windows_dlldir32\\opengl32.dll":
         ensure => present,
         backup => main,
         owner => Administrators,
