97ac9b740450d50e9677cdde99dd443b0e946d9e
Rohan McGovern <rohan.mcgovern@nokia.com>
2012-08-20 11:01:14 +1000

    baselayout: clean up testuser's TEMP on Windows
    Otherwise it can fill up with temporary files from crashing tests.
    
    Change-Id: I0347d9a755866ad9dca9d231290b40bb6e0393b6
    Reviewed-by: Toby Tomkins <toby.tomkins@nokia.com>

--- puppet/modules/baselayout/manifests/windows.pp
+++ puppet/modules/baselayout/manifests/windows.pp
@@ -1,5 +1,13 @@
 class baselayout::windows inherits baselayout::base {
     if $baselayout::testuser {
+        # clean testuser's temp periodically; if we don't, then nothing will clean
+        # up temporary files/directories from crashing/hanging tests
+        tidy { "C:\\Users\\$baselayout::testuser\\AppData\\Local\\Temp":
+            age => "1w",
+            recurse => true,
+            rmdirs => true,
+        }
+
         Git::Config {
             user => $baselayout::testuser,
         }
