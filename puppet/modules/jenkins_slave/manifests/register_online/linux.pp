3214aa0042f25ee04e5c6d1a51019e50d637b460
Rohan McGovern <rohan.mcgovern@nokia.com>
2012-10-03 15:09:28 +1000

    jenkins_slave: retry online-node a few times at startup
    When a test machine boots, it might happen that the
    jenkins_slave::register_online startup item is launched before the
    machine's network interface has been brought up, or the Jenkins server
    might be temporarily unreachable for some other reason.
    
    To be robust in this case, we should retry the online-node command a few
    times; otherwise the slave may remain in offline state for longer than
    necessary.
    
    Change-Id: I11694f0f49c6562f15981e6891613ffc388b2965
    Reviewed-by: Sergio Ahumada <sergio.ahumada@digia.com>
    Reviewed-by: Janne Anttila <janne.anttila@digia.com>

--- puppet/modules/jenkins_slave/manifests/register_online/linux.pp
+++ puppet/modules/jenkins_slave/manifests/register_online/linux.pp
@@ -3,7 +3,7 @@ class jenkins_slave::register_online::linux {
         path    =>  "/bin/sh",
         arguments => [
             "-c",
-            "/home/$jenkins_slave::user/jenkins/jenkins-cli.pl -- online-node $jenkins_slave::slave_name 2>&1 | tee /home/$jenkins_slave::user/jenkins/$jenkins_slave::cli_log | logger -t jenkins"
+            "/home/$jenkins_slave::user/jenkins/jenkins-cli.pl --retry -- online-node $jenkins_slave::slave_name 2>&1 | tee /home/$jenkins_slave::user/jenkins/$jenkins_slave::cli_log | logger -t jenkins"
         ],
         require =>  File["jenkins cli script"],
         user    =>  $jenkins_slave::user,
--- puppet/modules/jenkins_slave/manifests/register_online/mac.pp
+++ puppet/modules/jenkins_slave/manifests/register_online/mac.pp
@@ -3,7 +3,7 @@ class jenkins_slave::register_online::mac {
         path    =>  "/bin/sh",
         arguments => [
             "-c",
-            "/Users/$jenkins_slave::user/jenkins/jenkins-cli.pl -- online-node $::hostname 2>&1 | tee /Users/$jenkins_slave::user/jenkins/$jenkins_slave::cli_log | logger -t jenkins"
+            "/Users/$jenkins_slave::user/jenkins/jenkins-cli.pl --retry -- online-node $::hostname 2>&1 | tee /Users/$jenkins_slave::user/jenkins/$jenkins_slave::cli_log | logger -t jenkins"
         ],
         require =>  File["jenkins cli script"],
         user    =>  $jenkins_slave::user,
