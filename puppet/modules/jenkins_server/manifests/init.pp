7e511e603492fb17c41a8dcc5a76d61469cfaf71
Samuli Piippo <samuli.piippo@digia.com>
2012-10-05 09:05:58 +0300

    jenkins: Support installing jenkins server in Ubuntu
    The debian.pp works in Ubuntu as well.
    
    Change-Id: I32526f29ce11c12ab2a3235e62bf17e8d825f300
    Reviewed-by: Rohan McGovern <rohan.mcgovern@nokia.com>
    Reviewed-by: Janne Anttila <janne.anttila@digia.com>

--- puppet/modules/jenkins_server/manifests/init.pp
+++ puppet/modules/jenkins_server/manifests/init.pp
@@ -4,6 +4,7 @@ class jenkins_server (
     $apache2_frontend = true
 ) {
     case $::operatingsystem {
+        Ubuntu: { include jenkins_server::debian }
         Debian: { include jenkins_server::debian }
         default: {
             alert( "No implementation for jenkins_server on $::operatingsystem" )
