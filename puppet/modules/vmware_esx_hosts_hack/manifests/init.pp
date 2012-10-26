e37f6e5b22435a3d21e42243955c06c1a99de448
Janne Anttila <janne.anttila@digia.com>
2012-10-26 17:50:41 +0300

    Remove outdated vmware_esx_hosts_hack module.
    This module has been private and been used only in Nokia hosted CI system,
    because it is not used anymore it is safe to remove it.
    
    Change-Id: I1ee2b2103172f9f7b7d1df30f0546c776494581b
    Reviewed-by: Samuli Piippo <samuli.piippo@digia.com>
    Reviewed-by: Sergio Ahumada <sergio.ahumada@digia.com>

--- puppet/modules/vmware_esx_hosts_hack/manifests/init.pp
+++
@@ -1,5 +0,0 @@
-class vmware_esx_hosts_hack {
-    # This is a no-op; there is a private version of this module which contains
-    # some `hosts' file entries for some Nokia internal vmware ESX servers due to
-    # a DNS issue on Nokia LAN.  You don't need to care about it ^_^
-}
