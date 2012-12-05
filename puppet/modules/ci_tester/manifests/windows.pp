af51b6a00451e38278c72976da41af2731184486
Janne Anttila <janne.anttila@digia.com>
2012-12-05 08:49:28 +0200

    Enable control of python, mingw, openssl, perl and jom with puppet.
    Puppet configurations for the above mentions SWs were created earlier,
    but they were not included anywhere so they were not used.
    
    Added needed include statements to ci_tester windows configuration.
    
    Change-Id: Ib65391a3e3fc6e65438d988f0e57e3322e3c7a58
    Reviewed-by: Simo Fält <simo.falt@digia.com>
    Reviewed-by: Samuli Piippo <samuli.piippo@digia.com>
    Reviewed-by: Heikki Halmet <heikki.halmet@digia.com>

--- puppet/modules/ci_tester/manifests/windows.pp
+++ puppet/modules/ci_tester/manifests/windows.pp
@@ -1,3 +1,8 @@
 class ci_tester::windows inherits ci_tester::base {
     include mesa3d
+    include activepython
+    include mingw
+    include openssl
+    include strawberryperl
+    include jom
 }
