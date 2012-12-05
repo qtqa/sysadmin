157e3e3c76d7eea49861092750da2fc4c3e81f07
Janne Anttila <janne.anttila@digia.com>
2012-12-05 08:38:31 +0200

    Fix strawberryperl puppet configuration for Windows.
    Strawberryperl puppet configuration has not been used yet, because
    it is not included anywhere. When trying to include strawberryperl
    configuration I found the following errors:
    
    1. Binary is placed to '$path\\perl\\bin\\perl.exe' instead of
      '$path\\bin\\perl.exe'.
    2. Strawberryperl output version as follows: '(v5.14.2)', this does not
       match with currently used version expression which expects word
       boundaries around '5.14.2'.
    3. When testing Strawberryperl configuration further I found out that
       it is not possible to install a newer perl version to same path with
       previous installation. It is not possible because puppet is executed
       from sync_and_run.pl which uses perl and locks the used files.
       -> Added comment about this restriction.
    
    Without fixes 1 and 2 perl is reinstalled everytime puppet is executed.
    
    Change-Id: I5b6304488f3430a81f9c656679268ff839e7a3f6
    Reviewed-by: Samuli Piippo <samuli.piippo@digia.com>
    Reviewed-by: Simo Fält <simo.falt@digia.com>
    Reviewed-by: Heikki Halmet <heikki.halmet@digia.com>

--- puppet/modules/strawberryperl/manifests/windows.pp
+++ puppet/modules/strawberryperl/manifests/windows.pp
@@ -1,5 +1,7 @@
 # Downloads the given $version of strawberryperl from http://strawberryperl.com and installs to the specified $path.
 # If a different strawberryperl version is already installed there, it is uninstalled first.
+
+# Major caveat: Changing $version alone does not work because uninstalling $path when it is used fails
 class strawberryperl::windows(
     $version = '5.14.2.1',
     $path = 'C:\strawberry'
@@ -13,15 +15,12 @@ class strawberryperl::windows(
     # perl versions without build part
     $version_no_buildpart = regsubst($version, '^(\d+)\.(\d+)\.(\d+).(\d+)$', '\1.\2.\3')
 
-    # perl outputs version without build part
-    $version_expression = "\\b$version_no_buildpart\\b"
-
     windows::msi_package { "strawberryperl":
         url => $url,
         version => $version,
-        version_expression => $version_expression,
+        version_expression => $version_no_buildpart,
         install_flags => "/QB",
         path => $path,
-        binary => "$path\\bin\\perl.exe"
+        binary => "$path\\perl\\bin\\perl.exe"
     }
 }
