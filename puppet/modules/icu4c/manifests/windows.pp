8792c3f41c720bcfdef8c2f75f8b073a383ee8d6
Janne Anttila <janne.anttila@digia.com>
2012-12-10 15:49:05 +0200

    Add support for 64-bit operating systems to puppet icu4c configuration.
    QtCore build on 64-bit Windows 8 machine with MSVC2012 fails with
    32-bit ICU. Manual installation of 64 bit ICU made build to proceed
    futher => Automate it correctly.
    
    Change-Id: Ia73352eefb0af19ca8db01ee5d0dfbbb453d02cb
    Reviewed-by: Heikki Halmet <heikki.halmet@digia.com>
    Reviewed-by: Simo Fält <simo.falt@digia.com>
    Reviewed-by: Samuli Piippo <samuli.piippo@digia.com>

--- puppet/modules/icu4c/manifests/windows.pp
+++ puppet/modules/icu4c/manifests/windows.pp
@@ -7,13 +7,23 @@ class icu4c::windows(
     # Version number with underscores, for example '49_1_2'
     $version_underscore = regsubst($version, '\.', '_', "G")
 
+    $os_bits = $::architecture ? {
+        x64 => "Win64",
+        default => "Win32"
+    }
+
+    $bin_folder = $::architecture ? {
+        x64 => "bin64",
+        default => "bin"
+    }
+
     # installer file URL
-    $url = "http://download.icu-project.org/files/icu4c/${version}/icu4c-${version_underscore}-Win32-msvc10.zip"
+    $url = "http://download.icu-project.org/files/icu4c/${version}/icu4c-${version_underscore}-${os_bits}-msvc10.zip"
 
     windows::zip_package { "icu4c":
         url => $url,
         version => $version,
         path => $path,
-        binary => "$path\\icu\\bin\\icuinfo.exe"
+        binary => "$path\\icu\\${bin_folder}\\icuinfo.exe"
     }
 }
