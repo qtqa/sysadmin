9d12bf895e18fd8a723f218783f9b9b08cb95c3d
Tony Sarajarvi <tony.sarajarvi@digia.com>
2013-09-10 10:33:29 +0300

    Remove TestCocoon from Puppet's ci_tester runs
    The tool is not used, and caused problems in our
    Puppet runs. The URL Puppet tried to reach was unavailable.
    
    Change-Id: I22ae64b9e5dfb4d152913f3bc1ea9e69bfc2b5d3
    Reviewed-by: Sergio Ahumada <sergio.ahumada@digia.com>

--- puppet/modules/ci_tester/manifests/params.pp
+++ puppet/modules/ci_tester/manifests/params.pp
@@ -15,14 +15,12 @@ class ci_tester::params {
     $distcc_hosts = 'localhost'
 
     if ($::operatingsystem == 'Ubuntu') and ($::operatingsystemrelease == '11.10') {
-        $testcocoon_enabled = true
         if $::architecture == 'i386' {
             $armel_cross_enabled = true
         } else {
             $armel_cross_enabled = false
         }
     } else {
-        $testcocoon_enabled = false
         $armel_cross_enabled = false
     }
 
