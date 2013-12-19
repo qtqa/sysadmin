088e0e4c289dff45777425376623a7f148c145c9
Sergio Ahumada <sahumada@blackberry.com>
2013-12-19 14:38:40 +0100

    baselayout: Fix "Dynamic lookup is deprecated" warning
      puppet-apply[10893]: Dynamic lookup of $qtgitreadonly at /var/qtqa/sysadmin/puppet/modules/baselayout/manifests/base.pp:8 is deprecated.
        Support will be removed in Puppet 2.8.
        Use a fully-qualified variable name (e.g., $classname::variable) or parameterized classes.
      puppet-apply[10893]: Dynamic lookup of $location at /var/qtqa/sysadmin/puppet/modules/baselayout/manifests/base.pp:9 is deprecated.
        Support will be removed in Puppet 2.8.
        Use a fully-qualified variable name (e.g., $classname::variable) or parameterized classes.
      puppet-apply[10893]: Dynamic lookup of $qtgitreadonly at /var/qtqa/sysadmin/puppet/modules/baselayout/manifests/base.pp:10 is deprecated.
        Support will be removed in Puppet 2.8.
        Use a fully-qualified variable name (e.g., $classname::variable) or parameterized classes.
      puppet-apply[10893]: Dynamic lookup of $location at /var/qtqa/sysadmin/puppet/modules/baselayout/manifests/base.pp:11 is deprecated.
        Support will be removed in Puppet 2.8.
        Use a fully-qualified variable name (e.g., $classname::variable) or parameterized classes.
    
    Change-Id: I82e186df5f815706b72aa4030e3e575adb3ddcad
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@digia.com>

--- puppet/modules/baselayout/manifests/base.pp
+++ puppet/modules/baselayout/manifests/base.pp
@@ -5,10 +5,10 @@ class baselayout::base {
         }
 
         git::config {
-            "url.$qtgitreadonly.insteadof": content => "qtgitreadonly:";
-            "qtqa.hardgit.location": content => $location;
-            "qtqa.hardgit.server.qtgitreadonly.primary": content => $qtgitreadonly;
-            "qtqa.hardgit.server.qtgitreadonly.mirror-$location": content => $qtgitreadonly_local;
+            "url.$::qtgitreadonly.insteadof": content => "qtgitreadonly:";
+            "qtqa.hardgit.location": content => $::location;
+            "qtqa.hardgit.server.qtgitreadonly.primary": content => $::qtgitreadonly;
+            "qtqa.hardgit.server.qtgitreadonly.mirror-$::location": content => $qtgitreadonly_local;
             "user.name": content => "Qt Continuous Integration System";
             "user.email": content => "ci-noreply@qt-project.org";
             "http.postBuffer": content => "524288000";
