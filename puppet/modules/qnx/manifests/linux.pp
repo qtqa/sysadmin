985422f4c0e45b99a01ed9b08a88833ec5ff9ba9
Tony Sarajärvi <tony.sarajarvi@digia.com>
2014-03-28 09:41:12 +0200

    Fix Puppet's QNX module to prevent reinstall
    Change-Id: I48d4048ffa29c1d3810f209558951e00b45db8c5
    Reviewed-by: Heikki Halmet <heikki.halmet@digia.com>

--- puppet/modules/qnx/manifests/linux.pp
+++ puppet/modules/qnx/manifests/linux.pp
@@ -4,30 +4,41 @@ class qnx::linux
 {
     $url = "$input/qnx"
     $filename = "qnx660.tar.gz"
-    $target = "/opt"
+    $target = "/opt/qnx660"
 
-    file { "$target":
+    file { "/opt":
         ensure  =>  directory,
         owner   =>  root,
         group   =>  users,
         mode    =>  0755,
     }
 
+    file { "$target":
+        ensure  =>  directory,
+        owner   =>  qt,
+        group   =>  users,
+        mode    =>  0755,
+    }
+
     define qnx_install($filename,$options,$target,$url) {
-        exec { "install ${filename} to ${target}":
-            command =>  "/bin/bash -c 'wget $url/$filename -O - | tar -C $target -$options'",
-            unless => "/bin/bash -c 'if [ -e ${target} ]'",
-            require =>  File["$target"],
-            timeout =>  1800,
+        exec { "install $filename to $target":
+            command => "/bin/bash -c 'wget $url/$filename -O - | tar -C $target -$options'",
+            unless  => "/bin/bash -c '/usr/bin/test -d $target'",
+            require => File["/opt","$target"],
+            timeout => 1800,
         }
     }
 
-    qnx_install {
-        "sdp":
-            filename    =>  "$filename",
-            options     =>  "xz",
-            target      =>  "$target",
-            url         =>  "$url",
-        ;
+    if $::lsbmajdistrelease == 12 {
+        if $::architecture == amd64 {
+            qnx_install {
+                "sdp":
+                    filename => "$filename",
+                    options  => "xz --strip-components=1",
+                    target   => "$target",
+                    url      => "$url",
+                 ;
+            }
+        }
     }
 }
