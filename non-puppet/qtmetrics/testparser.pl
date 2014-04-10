89af95f8473ba3ebaac939aee015be237214f829
Tony Sarajärvi <tony.sarajarvi@digia.com>
2014-04-10 08:16:34 +0300

    Testparser fix for catching qt.tests.insignificant
    The script didn't catch the phrase "Note: qt.tests.insignificant
    is set, but the tests succeeded." thus leaving the SQL table
    with incorrect data in these cases.
    
    Change-Id: I425ceb9328b92902809e46073c1eba12152b035a
    Reviewed-by: Simo Fält <simo.falt@digia.com>

--- non-puppet/qtmetrics/testparser.pl
+++ non-puppet/qtmetrics/testparser.pl
@@ -403,6 +403,7 @@ sub get_log_data
     $logdata{FORCESUCCESS} = exists_in_array(\@filecontent, qr/^Normally I would now fail.  However, `forcesuccess' was set in/);
     $logdata{FORCESUCCESS} |= exists_in_array(\@filecontent, qr/^Note: forcesuccess is set, but the test script succeeded./);
     $logdata{INSIGNIFICANT} = exists_in_array(\@filecontent, qr/^This is a warning, not an error, because the `qt.tests.insignificant' option was used./);
+    $logdata{INSIGNIFICANT} |= exists_in_array(\@filecontent, qr/^Note: qt.tests.insignificant is set, but the tests succeeded./);
     return (\%logdata);
 }
 
