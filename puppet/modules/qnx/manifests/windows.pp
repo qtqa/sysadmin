7425bea77e2fca01fb3582b9dbc0a2b626feb2fc
Tony Sarajärvi <tony.sarajarvi@digia.com>
2014-05-15 15:18:35 +0300

    Add QNX's SDK to be installed to Windows envs
    Change-Id: I2a2bcc7c2f62d35a6d599ae39fbef4c0b5f4b0ad
    Reviewed-by: Samuli Piippo <samuli.piippo@digia.com>

--- puppet/modules/qnx/manifests/windows.pp
+++ puppet/modules/qnx/manifests/windows.pp
@@ -7,6 +7,16 @@ class qnx::windows
     $url2 = "$input/qnx/$filename2"
     $target = "c:\\qnx660"
 
+    $filename3 = "qnx-sdk-for-apps-and-media-1.0-201402230339.exe"
+    $filename4 = "resp_sdk.conf"
+    $url3 = "$input/qnx/$filename3"
+    $url4 = "$input/qnx/$filename4"
+    $target2 = "c:\\qnx660\\install\\qnx-sdk"
+
+    $filename5 = "patch_660-3865.7z"
+    $url5 = "$input/qnx/$filename5"
+    $target3 = "c:\\qnx660"
+
     $msysbin = $::architecture ? {
         x64     => 'c:\Program Files (x86)\Git\bin',
         default => 'c:\Program Files\Git\bin'
@@ -17,12 +27,20 @@ class qnx::windows
 
     $install_package = "${tempdir}/$filename"
     $resp_file = "${tempdir}/$filename2"
+    $sdk_file = "${tempdir}/$filename3"
+    $sdk_resp_file = "${tempdir}/$filename4"
+    $patch_file = "${tempdir}/$filename5"
 
     $fetch_cmd = "\"${msysbin}\\curl.exe\" \"$url\" -L -o \"${install_package}\""
     $fetch_cmd2 = "\"${msysbin}\\curl.exe\" \"$url2\" -L -o \"${resp_file}\""
+    $fetch_cmd3 = "\"${msysbin}\\curl.exe\" \"$url3\" -L -o \"${sdk_file}\""
+    $fetch_cmd4 = "\"${msysbin}\\curl.exe\" \"$url4\" -L -o \"${sdk_resp_file}\""
+    $fetch_cmd5 = "\"${msysbin}\\curl.exe\" \"$url5\" -L -o \"${patch_file}\""
 
     # Install command install's java using install package from $tempdir
     $install_cmd = "start \"install\" /wait \"$install_package\" -i silent -f \"$resp_file\""
+    $install_cmd2 = "start \"install\" /wait \"$sdk_file\" -i silent -f \"$sdk_resp_file\""
+    $install_cmd3 = "start \"install\" /wait C:\\utils\\sevenzip\\7z.exe -y x \"$patch_file\" -o\"$target3\""
 
     define qnx_install($filename,$target,$fetch_cmd,$fetch_cmd2,$install_cmd) {
         exec { "install $filename to $target":
@@ -39,6 +57,24 @@ class qnx::windows
             fetch_cmd  => "$fetch_cmd",
             fetch_cmd2 => "$fetch_cmd2",
             install_cmd => "$install_cmd",
-         ;
+            before => Qnx_install["sdk"],
+        ;
+    }
+
+    qnx_install {
+        "sdk":
+            filename => "$filename2",
+            target   => "$target2",
+            fetch_cmd  => "$fetch_cmd3",
+            fetch_cmd2 => "$fetch_cmd4",
+            install_cmd => "$install_cmd2",
+            before => Exec["unzip_patch"],
+        ;
+    }
+    exec { "unzip_patch":
+        command => "C:\\Windows\\system32\\cmd.exe /V:ON /C \"$fetch_cmd5 && $install_cmd3\"",
+        creates => "$target\\target\\qnx6\\x86\\usr\\lib\\libfontconfig.so",
+        timeout => 1800,
     }
 }
+
