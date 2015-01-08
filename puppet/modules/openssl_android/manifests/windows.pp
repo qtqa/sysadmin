ea2ef129e5093577297608c1dfe76d52a6c61b81
Matti Paaso <matti.paaso@theqtcompany.com>
2015-01-08 10:20:02 +0200

    Updating windows openssl version for Android
    Openssl version updated from 1.0.0a into 1.0.1j.
    
    Change-Id: If2bbfe3691102b1a3ab53a4a82d6ab7bf375fec6
    Reviewed-by: Iikka Eklund <iikka.eklund@theqtcompany.com>
    Reviewed-by: Kai Koehne <kai.koehne@theqtcompany.com>
    Reviewed-by: Heikki Halmet <heikki.halmet@theqtcompany.com>

--- puppet/modules/openssl_android/manifests/windows.pp
+++ puppet/modules/openssl_android/manifests/windows.pp
@@ -1,20 +1,20 @@
 # Downloads the given $version of openssl-android-master from $input and installs it to the specified $path.
 # If a different openssl-android version is already installed there, it is uninstalled first.
 class openssl_android::windows(
-    $version = '1.0.0a',
+    $version = '1.0.1j',
     $path = 'C:\utils'
 ) {
     # Currently only 32-bit version can be found for openssl-android!
     if ($::architecture == "x86") {
 
         # installer file URL
-        $url = "${input}/windows/openssl-android-master.zip"
+        $url = "${input}/windows/platform_external_openssl-master.zip"
 
         unzip_package { "openssl_android":
             url => $url,
             version => $version,
-            version_flags => "${path}\\openssl-android-master\\openssl.version",
-            path => "${path}\\openssl-android-master",
+            version_flags => "${path}\\platform_external_openssl-master\\openssl.version",
+            path => "${path}\\platform_external_openssl-master",
             unzip_flags => "x -o$path",
             binary => "\"C:\\Program Files\\Git\\bin\\cat.exe\""
         }
